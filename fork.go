4a384cd28d434066185be50318589e2d23bcbe59
mohammad katoozi <k2527806@gmail.com>
2019-06-26 22:02:27 +0430

    create fork command not implement it

--- fork.go
+++ fork.go
@@ -1,1 +1,26 @@
 package pork
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// ForkCmd  is a command that will fork given repo on github
+var ForkCmd = &cobra.Command{
+	Use:   "fork",
+	Short: "fork a repo on Github",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) <= 0 {
+			log.Fatalln("You must provide repo")
+		}
+		if err := ForkRepo(args[0]); err != nil {
+			log.Fatalln("error when forking repo: ", err)
+		}
+	},
+}
+
+// ForkRepo will fork repo
+func ForkRepo(repo string) error {
+	return nil
+}
--- fork_test.go
+++ fork_test.go
@@ -1,1 +1,9 @@
 package pork
+
+import "testing"
+
+func TestForkRepo(t *testing.T) {
+	if err := ForkRepo("katoozi/pork"); err != nil {
+		t.Fail()
+	}
+}
