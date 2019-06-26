d3f8c54ad25d7cf7d0b3cc08f846afab821060fc
mohammad katoozi <k2527806@gmail.com>
2019-06-26 21:47:14 +0430

    create clone command not implement it

--- clone.go
+++ clone.go
@@ -1,1 +1,36 @@
 package pork
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// CloneCmd is a command that clone Github repo into destination
+var CloneCmd = &cobra.Command{
+	Use:   "clone",
+	Short: "clone repo from Github",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) <= 0 {
+			log.Fatalln("You must provide repo")
+		}
+		if err := CloneRepo(args[0], ref, create); err != nil {
+			log.Fatalln("error when clone repo: ", err)
+		}
+	},
+}
+
+// CloneRepo will clone github repo into destination
+func CloneRepo(repo string, ref string, shouldCreate bool) error {
+	return nil
+}
+
+var (
+	ref    string
+	create bool
+)
+
+func init() {
+	CloneCmd.PersistentFlags().StringVar(&ref, "ref", "", "specific refrence to check out")
+	CloneCmd.PersistentFlags().BoolVar(&create, "create", false, "create refrence if it does not exist.")
+}
--- clone_test.go
+++ clone_test.go
@@ -1,1 +1,9 @@
 package pork
+
+import "testing"
+
+func TestCloneRepo(t *testing.T) {
+	if err := CloneRepo("katoozi/pork", "develop", false); err != nil {
+		t.Fail()
+	}
+}
