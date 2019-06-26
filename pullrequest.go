90ca5ad06e7b4c22a23548f0aa539a60f538e504
mohammad katoozi <k2527806@gmail.com>
2019-06-26 22:14:27 +0430

    create pull request command not implement it

--- pullrequest.go
+++ pullrequest.go
@@ -1,1 +1,19 @@
 package pork
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// PullRequestCmd is a command that create pull request
+var PullRequestCmd = &cobra.Command{
+	Use:   "pullrequest",
+	Short: "Create Pull Request",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+// PullRequest is function that send pullrequest request
+func PullRequest(repo string) error {
+	return nil
+}
--- pullrequest_test.go
+++ pullrequest_test.go
@@ -1,1 +1,9 @@
 package pork
+
+import "testing"
+
+func TestPullRequest(t *testing.T) {
+	if err := PullRequest("myrepo"); err != nil {
+		t.Fail()
+	}
+}
