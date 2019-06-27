095e651d9ac638a4eec684f6427a8049924210f4
mohammad katoozi <k2527806@gmail.com>
2019-06-27 22:32:04 +0430

    implement search api

--- search.go
+++ search.go
@@ -1,7 +1,14 @@
 package pork
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/katoozi/go-devops-tools/nap"
 
 	"github.com/spf13/cobra"
 )
@@ -19,19 +26,52 @@ type SearchReponse struct {
 	IncompleteResults bool             `json:"incomplete_results"`
 }
 
+func (s *SearchReponse) ToString() string {
+	response := ""
+	for _, v := range s.Results {
+		response += fmt.Sprintf("%s\n", v.FullName)
+	}
+	return response
+}
+
 // SearchCmd is a subcommand of pork
 var SearchCmd = &cobra.Command{
 	Use:   "search",
 	Short: "search for github repositories by keyword",
 	Run: func(cmd *cobra.Command, args []string) {
-		reposList := SearchByKeyword(args)
-		for _, repo := range reposList {
-			fmt.Println(repo)
+		if err := SearchByKeyword(args); err != nil {
+			log.Fatalf("Search Failed: %s", err)
 		}
 	},
 }
 
 // SearchByKeyword will take slice of keywords and return the github repos
-func SearchByKeyword(keywords []string) []string {
-	return []string{"myrepo"}
+func SearchByKeyword(keywords []string) error {
+	return GitHubAPI().Call("search", map[string]string{
+		"query": strings.Join(keywords, "+"),
+	})
+}
+
+func SearchSuccess(resp *http.Response, _ interface{}) error {
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	searchResponse := SearchReponse{}
+	json.Unmarshal(content, &searchResponse)
+	fmt.Print(searchResponse.ToString())
+	return nil
+}
+
+func SearchDefaultRouter(resp *http.Response, _ interface{}) error {
+	return fmt.Errorf("status code %d", resp.StatusCode)
+}
+
+func GetSearchResource() *nap.RestResource {
+	searchRouter := nap.NewRouter()
+	searchRouter.DefaultRouter = SearchDefaultRouter
+	searchRouter.RegisterFunc(200, SearchSuccess)
+	search := nap.NewResource("/search/repositories?q={{.query}}", "GET", searchRouter)
+	return search
 }
